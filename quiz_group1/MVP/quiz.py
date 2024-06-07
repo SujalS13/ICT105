fabb0f74563dd9665bc66e722414f6aaa405ce7a
NandniS26 <ns009156@gmail.com>
2024-06-07 13:33:18 +0930

    edited a little

--- quiz_group1/MVP/quiz.py
+++ quiz_group1/MVP/quiz.py
@@ -106,7 +106,7 @@ def read_questions_from_file(filename):
     # Read the questions from a file
     try:
         with open(filename, 'r') as file:
-        content = file.read()
+            content = file.read()
     except FileNotFOundError:
         messagebox.showerror("Error",f"File{filename} not found.")
         return []
